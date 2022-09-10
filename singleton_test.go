64c63ab9026824d40f41d2700cae07ab407476bb
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2022-09-10 15:24:02 +0800

    create Singleton

---
+++ singleton_test.go
@@ -0,0 +1,17 @@
+package belajar_golang_logging
+
+import (
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+func TestSingleton(t *testing.T) {
+	logrus.Info("Hello World")
+	logrus.Warn("Hello World")
+
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+
+	logrus.Info("Hello World")
+	logrus.Warn("Hello World")
+}
